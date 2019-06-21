352fc9932c329d5e22d081ec09b9588a487bc6bb
Mike Tracz <miketracz@hotmail.com>
2019-06-21 14:35:58 +0000

    User dashboard page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
  #   password: ENV['GMAIL_PASSWORD']
  # }
 
-  config.action_mailer.default_url_options = test
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
