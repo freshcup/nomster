2cc6a6b30369854cb255ccddf797337cfd9a8aec
Mike Tracz <miketracz@hotmail.com>
2019-06-26 15:33:28 +0000

    working on aws bucket images

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :photos
   geocoded_by :address
   after_validation :geocode
+  has_many :comments
+  has_many :photos
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
