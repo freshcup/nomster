d90d74bf9e68c3da364a404a74c3aa1a100157bb
Mike Tracz <miketracz@hotmail.com>
2019-06-26 22:50:27 +0000

    debugging photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,6 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region: ['US East (Ohio)']
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
